e5969609f5a09adb613588b4906037d9d3734c98
tetsuwo <tetsuwo.oishi@gmail.com>
2014-07-13 21:36:30 +0900

    update test code

--- test/RequestHandlerTest.php
+++ test/RequestHandlerTest.php
@@ -4,13 +4,13 @@ class RequestHandlerTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstructor()
     {
-        $requestHandler = new RequestHandler();
+        $requestHandler = new VoiceText\API\RequestHandler();
         $this->assertInstanceOf('VoiceText\API\RequestHandler', $requestHandler);
     }
 
     public function testSetApiKey()
     {
-        $requestHandler = new RequestHandler();
+        $requestHandler = new VoiceText\API\RequestHandler();
         $requestHandler->setApiKey('API_TOKEN');
         $this->assertAttributeEquals('API_TOKEN', 'apiKey', $requestHandler);
     }
